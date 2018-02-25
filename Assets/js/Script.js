56250be2cc7f4d089d79e9a7cd15412a2f1dcd78
alexbponder@gmail.com <alexbponder@gmail.com>
2018-02-25 13:14:29 -0800

    finished push

--- Assets/js/Script.js
+++ Assets/js/Script.js
@@ -1,13 +1,24 @@
 // Declare x, give it the value of 5
 // Declare x, give it the value of 5
 
-function Open() {
+function OpenPortfolio() {
 
-    urlval = "http://twitter.com"
+    urlval = "Portfolio.html"
     newWin = window.open(urlval, 'newHWin');
     newWin.focus();
     }
+    function OpenContact() {
 
+        urlval = "Contact.html"
+        newWin = window.open(urlval, 'newHWin');
+        newWin.focus();
+        }
+    function OpenAbout() {
+
+        urlval = "Index.html"
+        newWin = window.open(urlval, 'newHWin');
+        newWin.focus();
+        }
 
 
 
